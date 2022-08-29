8dd0968e4904a99fc94c71773ca67729b72de790
AkhatorEnosa <aosakhogba@gmail.com>
2022-08-29 17:11:10 +0100

    making the skill component better

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,32 +18,32 @@ const Skills = () => {
         </div>
         
         <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-6'>
-           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
+           <div className='md:w-[200px] mx-auto shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
             <img className="w-16 mx-auto" src={HTML} alt="HTML logo" />
             <p className='my-4'>HTML</p>
            </div>
 
-           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
+           <div className='md:w-[200px] mx-auto shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
             <img className="w-16 mx-auto" src={CSS} alt="CSS logo" />
             <p className='my-4'>CSS</p>
            </div>
 
-           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
+           <div className='md:w-[200px] mx-auto shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
             <img className="w-16 mx-auto" src={TW} alt="Tailwind logo" />
             <p className='my-4'>Tailwind</p>
            </div>
 
-           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
+           <div className='md:w-[200px] mx-auto shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
             <img className="w-16 mx-auto" src={JS} alt="JS logo" />
             <p className='my-4'>JavaScript</p>
            </div>
 
-           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
+           <div className='md:w-[200px] mx-auto shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
             <img className="w-16 mx-auto" src={REACT} alt="React logo" />
             <p className='my-4'>React</p>
            </div>
 
-           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
+           <div className='md:w-[200px] mx-auto shadow-md shadow-[#040c16] hover:scale-110 duration-300 rounded-lg '>
             <img className="w-16 mx-auto" src={GITHUB} alt="Github logo" />
             <p className='my-4'>Github</p>
            </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Work = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f] text-[#7d7f84]'>
+    <div name="work" className='w-full h-screen bg-[#0a192f] text-[#7d7f84] '>
       
     </div>
   );
